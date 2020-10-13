f1997817f2c94bdb06e37ceda1f39e1d82b2b8f8
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-10-14 00:28:17 +0200

    Use tuple for partition cache key

--- src/Extensions.Caching.Extras/MemoryCachePartition.cs
+++ src/Extensions.Caching.Extras/MemoryCachePartition.cs
@@ -37,43 +37,7 @@ namespace Extensions.Caching.Extras
             // Do NOT dispose the underlying IMemoryCache, as there might be other cache wrappers using it
         }
 
-        private CacheKey CreateCacheKey(object key) => new CacheKey(PartitionKey, key);
-
-        private struct CacheKey : IEquatable<CacheKey>
-        {
-            private readonly object _partitionKey;
-            private readonly object _key;
-
-            public CacheKey(object partitionKey, object key)
-            {
-                _partitionKey = partitionKey ?? throw new ArgumentNullException(nameof(partitionKey));
-                _key = key ?? throw new ArgumentNullException(nameof(key));
-            }
-
-            public bool Equals(CacheKey other)
-            {
-                return _partitionKey.Equals(other._partitionKey) && _key.Equals(other._key);
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                return obj is CacheKey cacheKey && Equals(cacheKey);
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    return (_partitionKey.GetHashCode() * 397) ^ _key.GetHashCode();
-                }
-            }
-
-            public override string ToString()
-            {
-                return $"{_partitionKey}/{_key}";
-            }
-        }
+        private object CreateCacheKey(object key) => (PartitionKey, key);
     }
 
     public class MemoryCachePartition<TPartition> : MemoryCachePartition, IMemoryCachePartition<TPartition>
